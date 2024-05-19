f92b81fefd94fe379b20aba7c900c5dd82e6ecb4
vishal talreja <167596359+Vishal2117@users.noreply.github.com>
2024-05-19 15:36:14 +0530

    data types
    also covered typeof operator and how to convert string into number and vise versa.

---
+++ 08.js
@@ -0,0 +1,32 @@
+//typeof operator
+
+//data types (primitive data types)
+//string = "vishal";
+//number = 21 17.06;
+//booleans
+//null 
+//undefined
+//BigInt
+//symbol
+
+let age = 21;
+let firstName = "vrusti";
+// console.log(typeof age);
+// console.log(typeof firstName);
+
+//convert number to string
+
+//to convert num into str we simpily add num with empty str
+
+// console.log(typeof (age + ""));
+
+// console.log(typeof(String(age)));//we also use String() method to convert num into str
+
+
+//convert string to number
+
+//to convert str into num we simpily write + starting of variables 
+
+// console.log(typeof +firstName);
+
+// console.log(typeof(Number(firstName)));//we also use Number() method to convert str into num
