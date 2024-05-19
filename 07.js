b01a95c7a0fc9fe8c3e56dbe1493bbae24fe9788
vishal talreja <167596359+Vishal2117@users.noreply.github.com>
2024-05-19 15:33:47 +0530

    string methods

---
+++ 07.js
@@ -0,0 +1,34 @@
+// trim()
+// toUpperCase()
+// tolowerCase()
+// slice 
+
+
+//trim()
+// let string = "   vishal   ";
+// console.log(string.length);
+// let newString = string.trim();//this will give a new string
+// console.log(newString)//vishal
+// console.log(newString.length)//6
+//in trim() when we use this we got new string because strings are immutale it cannot change the original string it gives updated string by removing spaces
+
+
+//toUpperCase()
+// string = string.toUpperCase();
+// console.log(string);
+
+// let str = "VISHAL";
+// str = str.toLowerCase();
+// console.log(str)
+
+// in toUpperCase() and toLowerCase() when we use this we got new string because strings are immutale it cannot change the original string it gives updated string by removing spaces
+
+//silce
+// start insex 
+//end index
+//silce() not including end index but it runs upto it
+
+let str = "vishal";
+
+let newString = str.slice(0,4);//vish
+console.log(newString);
