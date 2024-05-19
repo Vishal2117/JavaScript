008038e36d53ed6127e84d45a7ec61dd83342ee9
vishal talreja <167596359+Vishal2117@users.noreply.github.com>
2024-05-19 15:37:23 +0530

    template string

---
+++ 10.js
@@ -0,0 +1,12 @@
+//template string
+
+let age = 21;
+let str = "vishal";
+
+// my age is 21 and my name is vishal.
+
+// let aboutMe = "my age is " + age + " and my name is "+ str +".";
+// console.log(aboutMe);
+
+let aboutMe = `my age is ${age} and my name is ${str}.`
+console.log(aboutMe);
