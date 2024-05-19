d5693a57e86ca9c40eb4b180f49c053b986fb4ca
vishal talreja <167596359+Vishal2117@users.noreply.github.com>
2024-05-19 15:32:47 +0530

    string indexing

---
+++ 06.js
@@ -0,0 +1,17 @@
+// string indexing
+let str = "v i s h a l";
+let str1 = "vishal";
+let str2 = "hvkdjjdkduvhdkvvnhdvvjdl";
+
+// want to find length of string we use length method and this method counts spaces also
+
+console.log(str1[2]);//the output of string is s
+console.log(str1.length); //the output of string is 6
+console.log(str.length); //the output of string is 11
+
+// last index : length - 1
+// index is always count from zero
+
+// if you wnat print last index of your string but you don't know the last index of string simply do this 
+// console.log(str2[str2.length-1]);
+ 
