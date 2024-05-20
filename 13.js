64bc960c25e1b7d20753fb25eda3ec464e0810b9
vishal talreja <167596359+Vishal2117@users.noreply.github.com>
2024-05-20 09:06:03 +0530

    truthy and falsy values

---
+++ 13.js
@@ -0,0 +1,31 @@
+//truthy and falsy values
+
+//falsy values
+
+// false
+// ""
+// null
+// undefined
+// 0
+
+//when you initliaze falsy values in variables it return false always
+let firstName = null;
+
+if(firstName){
+    console.log("inside if");
+}else{
+    console.log("inside else");//output
+}
+
+//truthy values
+
+//vishal
+//21
+
+let str = 22;
+
+if(str){
+    console.log(str);//output
+}else{
+    console.log("inside else");
+}
