3e29c4e7c4ca6beaabe0ae9b43ef24aceb6304e0
vishal talreja <167596359+Vishal2117@users.noreply.github.com>
2024-05-20 09:06:37 +0530

    if else condition

---
+++ 14.js
@@ -0,0 +1,17 @@
+//if else condition
+
+// let age = 19;
+
+// if(age>18){
+//     console.log("You're eligible for vote.");
+// }else{
+//     console.log("You're not eligible for vote.");
+// }
+
+let num = 19;
+
+if(num%2==0){
+    console.log("even");
+}else{
+    console.log("odd");
+}
