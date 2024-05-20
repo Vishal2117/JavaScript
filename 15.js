f9beef32bce80e36eb9cb043f63d15f49ac6b7b1
vishal talreja <167596359+Vishal2117@users.noreply.github.com>
2024-05-20 09:07:07 +0530

    ternary operator

---
+++ 15.js
@@ -0,0 +1,17 @@
+//ternary operator / conditional operator
+
+let age = 12;
+
+// let drink;
+
+// if(age>10){
+//     drink = "coffee";
+// }else{
+//     drink = "milk";
+// }
+// console.log(drink);
+
+
+let person = age > 10 ? "coffee" : "milk";
+
+console.log(person);
