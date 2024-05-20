5c9fa1436b60ff6082d7ade47985a1ab4e685981
vishal talreja <167596359+Vishal2117@users.noreply.github.com>
2024-05-20 09:05:17 +0530

    booleans & comparison operators

---
+++ 12.js
@@ -0,0 +1,18 @@
+//booleans & comparison operators
+
+//booleans
+//true, false
+
+let num = "4";
+let num1 = 4;
+
+// console.log(num<num1)
+
+//== or ===
+
+// console.log(num == num1);//this operator check only value not check data types
+// console.log(num === num1);//this operator check both data types and values
+
+//!= or !==
+
+console.log(num != num1);
