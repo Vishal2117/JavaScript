caadf86121d8a71d37ab9e3d3e198541d588ef52
vishal talreja <167596359+Vishal2117@users.noreply.github.com>
2024-05-28 08:31:17 +0530

    And / OR operator

---
+++ 16.js
@@ -0,0 +1,34 @@
+//and or operator
+
+
+//and
+// let firstName = "Vishal";
+// let age = 21;
+
+// if(firstName[0] == "V"){
+//     console.log("youre name starts with V");
+// }
+
+// // if(age>18){
+//     console.log("youre above 18");
+// }
+
+// if(firstName[0] == "V" && age>18){
+//     console.log("Condition is true");
+// }else{
+//     console.log("Condition is false");
+// }
+//in this both the condition will check and one of them is false entire condition becomes false
+
+//or
+ let firstName = "Vrusti";
+
+ let age = 21;
+
+if(firstName[0] == "V" || age>18){
+        console.log("Condition is true");
+    }else{
+        console.log("Condition is false");
+    }
+//in this if both conditions are false then else block will run 
+//one condition is true the entire condition is true
