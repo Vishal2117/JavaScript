658618dcf0f6bfbdcce3a5e3a634829f708105c7
vishal talreja <167596359+Vishal2117@users.noreply.github.com>
2024-05-28 08:36:17 +0530

    break and continue keyword

---
+++ 24.js
@@ -0,0 +1,24 @@
+//break keyword and continue keyword
+
+//break keyword
+
+//this keyword break the iteration in mid
+
+// for(let i=0;i<9;i++){
+//     if(i==4){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+//continue keyword
+
+// this keyword check the condition if its true then it goes back in iteration and run the code
+
+//inshort this will skip the value that we put for comparing
+for(let i=0;i<9;i++){
+    if(i==4){
+        continue;
+    }
+    console.log(i);
+}
