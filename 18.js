22f40d50afa0b4455b4f08a5d2afa6cbcf187af5
vishal talreja <167596359+Vishal2117@users.noreply.github.com>
2024-05-28 08:32:28 +0530

    if else if

---
+++ 18.js
@@ -0,0 +1,16 @@
+//if else if 
+
+let age = +prompt("Enter your age:");
+
+if(age < 10){
+    console.log("Sorry you're kid");
+}
+else if(age < 15){
+    console.log("Sorry you're teen");
+}
+else if(age < 18){
+    console.log("Behave Yousrself you are not kid anymore.");
+}
+else if(age < 25){
+    console.log("Behave yourself you're now mature man.");
+}
