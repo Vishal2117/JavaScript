d0a25990d1e742d1302c4a57159c5382131e6538
vishal talreja <167596359+Vishal2117@users.noreply.github.com>
2024-05-28 08:34:12 +0530

    while loop example

---
+++ 21.js
@@ -0,0 +1,23 @@
+//while loop example
+
+//multiplication table 
+
+let n1 = +prompt("Enter the multiplicand:");
+let n2 = 1;
+
+
+while(n2<=10){
+    n3 = n1 * n2;
+    console.log(`${n1} x ${n2} = ${n3}`);
+    n2++;
+}
+
+
+// let total = 0;
+// let i = 0;
+
+// while(i<=10){
+//     total = total + i;
+//     i++;
+// }
+// console.log(total)
