76768a05ef58bab1db3320ff9246d5c0ccfa7a8f
vishal talreja <167596359+Vishal2117@users.noreply.github.com>
2024-05-28 08:36:55 +0530

    do while loop

---
+++ 25.js
@@ -0,0 +1,22 @@
+//do while loop
+
+
+// let i = 10;
+
+// do{
+//     console.log(i);
+//     i++;
+    
+// }while(i<9)
+
+//this loop runs atleast one time doesn't matter condition is true or not
+
+let n1 = 2;
+let n2 = 1;
+
+do{
+    n3 = n1 * n2;
+    console.log(`${n1} x ${n2} = ${n3}`);
+    n2++;
+}while(n2<=10)
+
