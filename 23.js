9f37fcfec638a1f77b3981ff50ad2d7cbe05d014
vishal talreja <167596359+Vishal2117@users.noreply.github.com>
2024-05-28 08:35:38 +0530

    for loop example

---
+++ 23.js
@@ -0,0 +1,21 @@
+//for loop examples
+
+
+
+let total = 0;
+
+for(let i=0;i<=10;i++){
+    total = total + i;
+}
+console.log(total);
+
+//multiplication table 
+
+// let n1 = +prompt("Enter the multiplicand:");
+// let n2 = 1;
+
+// for(;n2<=10;n2++){
+//     n3 = n1 * n2;
+//     console.log(`${n1} x ${n2} = ${n3}`);
+// }
+
