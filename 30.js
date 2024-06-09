3f71794be8a7cd86b532cec87e929c4acbe8b85c
vishal talreja <167596359+Vishal2117@users.noreply.github.com>
2024-06-09 15:20:54 +0530

    for loop in array

---
+++ 30.js
@@ -0,0 +1,9 @@
+//for loop in array
+
+let fruits = ["apple","banana","mango","orange"];
+let fruits2 = [];
+for(i=0;i<fruits.length;i++){
+    // console.log(fruits[i]);
+    console.log(fruits2.push(fruits[i].toUpperCase()));
+    // console.log(fruits[i].toUpperCase());
+}
