676e82096d63cf4ed4b60e58991b27f5ac0d7397
vishal talreja <167596359+Vishal2117@users.noreply.github.com>
2024-06-09 15:25:02 +0530

    objects in array

---
+++ 36.js
@@ -0,0 +1,28 @@
+//objects
+
+//objects is reference type
+//arrays are good but not sufficient
+//for real world data
+//objects store in key value pairs
+//objects dont have index
+
+//how to create object
+
+const obj = {
+    Name : "vishal",
+    age : 21,
+    Hobby : ["playing cricket","listening Music"]
+
+};
+// console.log(obj);
+
+//how to access data from objects
+
+console.log(obj.Name);
+console.log(obj.age);
+console.log(obj.Hobby[0]);
+console.log(obj.Hobby[1]);
+
+//how to add key value pair to objects
+obj.gender = "male";
+console.log(obj);
