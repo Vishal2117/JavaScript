0c007292ea33458b793ca5bfdae5399c1989b717
vishal talreja <167596359+Vishal2117@users.noreply.github.com>
2024-06-09 15:17:49 +0530

    Intro to arrays

---
+++ 26.js
@@ -0,0 +1,25 @@
+//intro to arrays
+//reference type
+//how to create arrays
+
+//ordered collection of items
+
+// let fruits = ["apple","mango","banana"];
+// let num = [1,2,34,4,5];
+// let mix = ["string",null,undefined,1,2,true];
+
+
+// console.log(fruits[0]);
+// console.log(num);
+// console.log(mix);
+
+let fruits = ["apple","mango","banana"];
+let obj = {};
+// console.log(fruits);
+// fruits[1] = "kiwi";
+// console.log(fruits);
+console.log(typeof fruits);
+console.log(typeof obj);
+console.log(Array.isArray(fruits));
+console.log(Array.isArray(obj));
+//isArray funtion is used for checking if it is array or not
