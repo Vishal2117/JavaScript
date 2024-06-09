ea64ec3f9ec61812d9d5b03b1b954a8dd7a65130
vishal talreja <167596359+Vishal2117@users.noreply.github.com>
2024-06-09 15:18:45 +0530

    Method of array

---
+++ 27.js
@@ -0,0 +1,26 @@
+//array push pop
+
+//array unshift shift
+
+let fruits = ["apple","mango","banana"];
+console.log(fruits);
+
+//push
+// let pushfruit = fruits.push("Kiwi");
+// console.log(fruits);
+// console.log(pushfruit);
+
+//pop
+// let poppedfruit = fruits.pop();
+// console.log(fruits);
+// console.log("poppedfruit = ",poppedfruit);
+
+//unshift
+// let unshiftfruit = fruits.unshift("Watermelon");
+// console.log(fruits);
+// console.log(unshiftfruit);
+
+//shift
+let removedfruit = fruits.shift();
+console.log(fruits);
+console.log("removedfruit = ",removedfruit);
