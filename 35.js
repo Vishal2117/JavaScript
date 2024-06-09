8ed42c14fac26224648e968d6464b8a5a8cda3ae
vishal talreja <167596359+Vishal2117@users.noreply.github.com>
2024-06-09 15:23:59 +0530

    array destructuring

---
+++ 35.js
@@ -0,0 +1,41 @@
+// array destructuring
+
+// const arr1 = ["value1","value2","value3"];
+
+// let myVar = arr1[0];
+// let myVar1 = arr1[1];
+// console.log("value of myVar:- ",myVar);
+// console.log("value of myVar1:- ",myVar1);
+
+// array destructuring 
+
+// let [myVar,myVar1,myVar2] = arr1;
+// console.log("value of myVar:- ",myVar);
+// console.log("value of myVar1:- ",myVar1);
+// console.log("value of myVar2:- ",myVar2);
+
+// skiping index
+
+// const arr1 = ["value1","value2","value3"];
+// let [myVar,,myVar2] = arr1;
+// console.log("value of myVar:- ",myVar);
+// console.log("value of myVar2:- ",myVar2);
+
+// when array has only one value
+
+// const arr1 = ["value1"];
+// let [myVar,myVar1,myVar2] = arr1;
+// console.log("value of myVar:- ",myVar);//value1
+// console.log("value of myVar1:- ",myVar1);//undefined
+// console.log("value of myVar2:- ",myVar2);//undefined
+
+// using spread operator
+
+// const arr1 = ["value1","value2","value3","value4"];
+
+// // let Newarr = arr1.slice(2);
+// let [myvar1,myvar2,...Newarr] = arr1;
+// console.log("value of Newarr: ",Newarr);
+// console.log("value of myVar1: ",myvar1);
+// console.log("value of myVar2: ",myvar2);
+
