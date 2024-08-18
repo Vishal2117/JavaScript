ef69d4d6cd7948b47d21cc6260d608808c51a20d
vishal talreja <167596359+Vishal2117@users.noreply.github.com>
2024-08-18 18:16:39 +0530

    filter method

---
+++ 58.js
@@ -0,0 +1,16 @@
+// filter method 
+
+// const numbers = [1,2,3,4,5,7,89,0];
+
+// const isEven = function(number){
+//     return number%2==0;
+// }
+// const evenNumbers = numbers.filter(isEven);
+// console.log(evenNumbers);
+
+// const isOdd = function(number){
+//     return number%2!=0;
+// }
+
+// const oddNumbers = numbers.filter(isOdd);
+// console.log(oddNumbers);
