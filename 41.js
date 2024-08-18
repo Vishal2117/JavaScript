74171a363b4e65a84dfe6de2f3643c4b991237cb
vishal talreja <167596359+Vishal2117@users.noreply.github.com>
2024-08-18 17:50:59 +0530

    object destructuring

---
+++ 41.js
@@ -0,0 +1,16 @@
+//object destructuring
+
+const band = {
+    bandName : "Diljit paaji band",
+    famousSong : "Love ya",
+    Est : 2010,
+    anotherFamousSong : "Lalkara"
+}
+
+// const {bandName:var1, famousSong:var2, ...restProps} = band;
+// const {bandName, famousSong} = band;
+// console.log(var1);
+// console.log(var2);
+// console.log(bandName);
+// console.log(famousSong);
+// console.log(restProps);
