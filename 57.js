93a544ed2428c17886855e964f47b91acd81c0be
vishal talreja <167596359+Vishal2117@users.noreply.github.com>
2024-08-18 18:15:55 +0530

    map method

---
+++ 57.js
@@ -0,0 +1,28 @@
+// map method
+
+// const numbers = [2,3,5,6];
+
+// const square = (number) => {
+//     return number *  number;
+// }
+
+// const squareNumbers = numbers.map(square);
+// console.log(squareNumbers);
+
+// const square = numbers.map((number)=>{
+//     return number * number;
+// });
+
+// console.log(square);
+
+// const users = [
+//     {firstName : "vishal", age: 21},
+//     {firstName : "joginder", age: 22},
+//     {firstName : "garima", age: 20}
+// ];
+
+// const userNamesAndAge = users.map((user)=>{
+//     return `My Name is ${user.firstName} and my age is ${user.age}.`
+// });
+
+// console.log(userNamesAndAge);
