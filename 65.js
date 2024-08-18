017dfe81763769979d44768dedb83215f8b0ea51
vishal talreja <167596359+Vishal2117@users.noreply.github.com>
2024-08-18 18:23:31 +0530

    splice method

---
+++ 65.js
@@ -0,0 +1,19 @@
+// splice method
+// start delete insert 
+
+// const myArray = ['item1','item2','item3','item4'];
+
+// // delete 
+// const deletedItem = myArray.splice(1,2);
+// console.log("deleted item:",deletedItem)
+// console.log(myArray);
+
+// insert
+
+// const insertItem = myArray.splice(3,0,"item5");
+// console.log(myArray);
+
+// //insert and delete
+// const deletedItem = myArray.splice(1,1,"item6");
+// console.log("deleted Item:-", deletedItem);
+// console.log(myArray);
