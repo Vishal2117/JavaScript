a9f38cc09a88b7d8a608deee48b0cacf153ceb03
vishal talreja <167596359+Vishal2117@users.noreply.github.com>
2024-08-18 18:22:55 +0530

    fill method

---
+++ 64.js
@@ -0,0 +1,9 @@
+// fill method
+// value start end
+
+// const myArray = new Array(10).fill(-1);
+// console.log(myArray);
+
+// const myArray = [1,2,3,4,5,6,7,8,9];
+// myArray.fill(17,2,4);
+// console.log(myArray);
