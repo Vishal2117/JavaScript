fa847bb8bb3ad1a3c50c745ddf0b366c3942d99a
vishal talreja <167596359+Vishal2117@users.noreply.github.com>
2024-08-18 18:13:45 +0530

    callback function

---
+++ 54.js
@@ -0,0 +1,27 @@
+//callback function
+
+// function func1(name){
+//     console.log("inside the Func1");
+//     console.log(`My Love of Life is ${name}`)
+// }
+
+// function func2(v){
+//     console.log("inside the func2");
+//     v("my mom");
+// } 
+
+// func2(func1);
+
+//just for practice
+
+// function sumOfTwoNumbers(num1,num2){
+//     console.log("inside question panel");
+//     return num1 + num2;
+// }
+
+// function displayAnswer(obj){
+//     console.log("inside answer panel");
+//     console.log(obj(2,8));
+// }
+
+// displayAnswer(sumOfTwoNumbers);
