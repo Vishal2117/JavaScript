4b68be62292eb425c6915c8adb9574878aadeb0b
vishal talreja <167596359+Vishal2117@users.noreply.github.com>
2024-08-18 18:22:20 +0530

    some method

---
+++ 63.js
@@ -0,0 +1,19 @@
+//some method
+
+// const numbers = [3,5,7,90,1];
+
+// kya ek bhi number aisa hai jo even hai 
+//return true
+
+// const ans = numbers.some((number)=>number%2===0);
+// console.log(ans);
+
+// const myCart = [
+//     {productId : 1, productName : "Mobile", price: 15000},
+//     {productId : 2, productName : "Laptop", price: 45000},
+//     {productId : 3, productName : "Refigrator", price: 10000},
+//     {productId : 4, productName : "Macbook", price: 25000},
+// ];
+
+// const ans = myCart.some((cartPrice)=>cartPrice.price > 200000);
+// console.log(ans);
