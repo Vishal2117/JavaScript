fe73527a09d819403609d75c58f27f630b935692
vishal talreja <167596359+Vishal2117@users.noreply.github.com>
2024-08-18 18:24:04 +0530

    iterables

---
+++ 66.js
@@ -0,0 +1,22 @@
+// iterables 
+// jispe hum for of loop laga sake
+// string , array are iterable
+
+// const firstName = "vishal";
+// for(let char of firstName){
+//     console.log(char);
+// }
+
+// const items = ['item1', 'item2','item3'];
+// for(let item of items){
+//     console.log(item);
+// }
+
+// array like object
+// jinke pass length property hai
+// aur jisko hum index se access kar sakte hai
+// example :- string
+
+// const firstName = "vishal";
+// console.log(firstName[0]);
+// console.log(firstName.length);
